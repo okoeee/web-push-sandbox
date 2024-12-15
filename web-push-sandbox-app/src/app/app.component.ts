a2b2dc3d6e418fa9501b22d62e0d97b1291390a2
okoeee <yk27.puxcm@gmail.com>
2024-12-15 22:51:14 +0900

    add subscription exec

--- web-push-sandbox-app/src/app/app.component.ts
+++ web-push-sandbox-app/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HttpClient} from '@angular/common/http';
+import {SwPush} from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +12,33 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'web-push-sandbox-app';
+  private publicKey = 'BAU8tTZozk7GwhJv3xm0b0wrEiW8_Bx5hDLnRYc1epSwpPei4hecZW4gvYKxj5Y7KoD6Wpjmo78Xjzei1n0bBbs'
+
+  constructor(
+    private http: HttpClient,
+    private swPush: SwPush
+  ) {}
+
+  ngOnInit(): void {
+    this.subscribeToPush()
+    this.swPush.messages.subscribe(message => {
+      console.log('Push message: ', message)
+    })
+  }
+
+  subscribeToPush() {
+    if(!this.swPush.isEnabled) {
+      console.warn('Push notifications are not enabled')
+      return
+    }
+
+    this.swPush
+      .requestSubscription({
+        serverPublicKey: this.publicKey
+      })
+      .then(subscription =>{
+        console.log('subscription: ', subscription.toJSON())
+      })
+  }
+
 }
